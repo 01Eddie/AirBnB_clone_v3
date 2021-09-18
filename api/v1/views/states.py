0544bbe53a86850d5dc25877df430a83f366bd99
01Eddie <jesus.11.97@hotmail.com>
2021-09-17 20:36:14 -0500

    [ADD] api/v1/cities.py and [ADD] line in __init__.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -4,7 +4,7 @@
 from models import storage
 from api.v1.views import app_views
 from flask import jsonify, abort, make_response, request
-
+from models.state import State
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def states():
