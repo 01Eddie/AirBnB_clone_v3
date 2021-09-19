c38384c47e7e2b810be1a22292a07140d61241a1
01Eddie <jesus.11.97@hotmail.com>
2021-09-19 00:12:33 -0500

    [FIX] pep8 amenities.py

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -18,7 +18,8 @@ def get_amenities():
     return jsonify(getListA)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
 def retrieve_Amenity(amenity_id):
     """get amenity by id"""
     amenity = storage.get("Amenity", amenity_id)
@@ -27,7 +28,8 @@ def retrieve_Amenity(amenity_id):
     return jsonify(amenity.to_dict())
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
 def amenity_delete(amenity_id):
     """delete a amenity"""
     amenity = storage.get("Amenity", amenity_id)
@@ -51,7 +53,8 @@ def amenity_post():
     return (Amenity(**kAmenity).to_dict()), 201
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def amenity_put(amenity_id):
     """update a amenity"""
     amenity = storage.get("Amenity", amenity_id)
